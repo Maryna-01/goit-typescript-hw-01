6b431b71f9c5f9dfbed77d5c1bcfe87de01aa17f
unknown <liashchenkomarina@gmal.com>
2024-11-16 21:47:03 +0100

    fix task

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,5 +1,10 @@
-function merge<T, U>(objA: T, objB: U): T & U {
-    return { ...objA, ...objB };
+
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+    return Object.assign(objA, objB) as T & U;
 }
+
+
+
+
 
 
